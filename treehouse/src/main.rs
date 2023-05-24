cef24d0afef6eb490f3a4c1c7ed0cb09b28bb7b3
Patrick Donnelly <47293932+pdonnelly3@users.noreply.github.com>
2023-05-23 23:19:02 -0700

    chapter 2 done

---
+++ treehouse/src/main.rs
@@ -0,0 +1,13 @@
+use std::io::stdin;
+
+fn main() {
+    println!("Hello, {:?}", what_is_your_name());
+    
+    let list = ["test"];
+}
+
+fn what_is_your_name() -> String {
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("Failed to read line");
+    name.trim().to_lowercase()
+}
